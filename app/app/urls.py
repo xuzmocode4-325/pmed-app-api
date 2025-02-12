0ffbd7dc42e6304278178b49d90c3cf68e871297
Katlego Koketso Thobye <katlegothobye@yahoo.com>
2025-02-12 05:03:53 +0200

    update admin site header

--- app/app/urls.py
+++ app/app/urls.py
@@ -21,6 +21,10 @@ from drf_spectacular.views import (
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = 'PelluMed Administration Portal'
+admin.site.site_title = 'Admin Site'
+admin.site.index_title = 'PelluMed Admin'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
