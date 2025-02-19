b7b2edc2fb64913a00fcc1021db53651ceb39a0f
Katlego Koketso Thobye <katlegothobye@yahoo.com>
2025-02-19 07:33:20 +0200

    rename staff procedure api test class

--- app/event/tests/test_procedure_api_staff.py
+++ app/event/tests/test_procedure_api_staff.py
@@ -20,19 +20,7 @@ def procedure_detail_url(procedure_id):
     """Create and return a procedure detail url"""
     return reverse('event:procedure-detail', args=[procedure_id])
 
-
-class PublicProceduresApiTest(TestCase):
-    """Test unauthenticated API requests"""
-
-    def setUP(self):
-        self.client = APIClient()
-
-    def test_auth_required(self):
-        """Test auth is required for retrieving procedures"""
-        res = self.client.get(PROCEDURES_URL)
-        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
-
-class DoctorProcedureApiTests(TestCase):
+class StaffProcedureApiTests(TestCase):
     """Test authenticated API requests"""
 
     def setUp(self):
